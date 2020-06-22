01f5f6da3797fdb3925f3932fe38c715b8d0b615
Jesper <jezper@hotmail.dk>
2020-06-22 08:15:51 +0200

    Begyndelsen
    på en måde

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Button,
   View,
-  SafeAreaView,
+  TextInput,
   Text,
   Alert,
 } from 'react-native';
 import Constants from 'expo-constants';
 
+const ApiKey ="4ca58c02db0e5bd08cba675c246b5762";
 
+const Bynavn_land = "";
 
+fetch('http://api.openweathermap.org/data/2.5/weather?q=viborg&appid=4ca58c02db0e5bd08cba675c246b5762')
 export default function App() {
   return (
-    <View>
-    <Text style={styles.text}>
+    <View style={styles.Continaer}>
+      <Text style={styles.text}>
       Her det en test jeg prøver
-     
-    </Text>
-
-    <Button
-          title="Press me"
-          onPress={() => Alert.alert('You are gay')}
-          backgroundColor=""
-        />
+      </Text>
+    <TextInput
+    style={styles.input}
+    placeholder=' awdwad'
+    />
     </View>
   );
 }
@@ -32,9 +32,15 @@ const styles = StyleSheet.create({
   text: {
     marginTop: Constants.statusBarHeight,
     textAlign: "center",
-    color: "red",
-    backgroundColor: "yellow",
-    padding: 30,
   },
-  
+  input:{
+    borderWidth: 1,
+    width: 200,
+    backgroundColor: "#E9EBEE",
+    color: "blue",
+  },
+  Continaer:{
+    alignItems: "center",
+    //justifyContent: "center",
+  },
 });

56bf448da8c3c0c9676ee9eafaea16f0735041c6
Jesper <jezper@hotmail.dk>
2020-06-22 12:05:33 +0200

    Kan få data fra vejert

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Button,
-  View,
-  TextInput,
-  Text,
-  Alert,
+import React, { useState, Component } from 'react';
+import { StyleSheet,Button,View,TextInput,Text, Alert,
+  
 } from 'react-native';
 import Constants from 'expo-constants';
 
-const ApiKey ="4ca58c02db0e5bd08cba675c246b5762";
 
-const Bynavn_land = "";
+export default class App extends Component {
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=viborg&appid=4ca58c02db0e5bd08cba675c246b5762')
-export default function App() {
+  getDataUsingGet(){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=viborg&appid=4ca58c02db0e5bd08cba675c246b5762')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+        
+    })
+    .catch((error) => {
+        alert(JSON.stringify(error));
+        console.error(error);
+    });
+  }
+
+  render() {
   return (
     <View style={styles.Continaer}>
       <Text style={styles.text}>
-      Her det en test jeg prøver
+        Skriv den by du ville gerne se vejert for.
       </Text>
+
     <TextInput
     style={styles.input}
-    placeholder=' awdwad'
+    placeholder=' Skriv et by navn'
     />
+
+      <Text style={styles.Vejertekst}>
+        Her er vejert for: 
+      </Text>
+      <Text>
+        
+      </Text>
+      <Button title='Get Data Using GET' onPress={this.getDataUsingGet}/>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   text: {
-    marginTop: Constants.statusBarHeight,
-    textAlign: "center",
+    marginTop: Constants.statusBarHeight + 10,
+    fontSize: 20,
   },
+
+  Vejertekst:{
+    fontSize: 20,
+  },
+
   input:{
     borderWidth: 1,
     width: 200,
     backgroundColor: "#E9EBEE",
-    color: "blue",
+    marginTop: 16,
   },
   Continaer:{
     alignItems: "center",
-    //justifyContent: "center",
+    justifyContent: "center",
+    
   },
 });
